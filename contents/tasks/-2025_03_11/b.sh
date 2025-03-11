276c7db4b39ad9510aee18cae5b9a2fce578d3fa
Vu Van Nghia <vuvannghia.work@gmail.com>
2025-03-12 00:31:00 +0700

    update 1 file and create 7 files

---
+++ contents/tasks/-2025_03_11/b.sh
@@ -0,0 +1,75 @@
+# # Xem tất cả số lượng
+# curl -X 'GET' \
+#   'http://localhost:9010/api/bss/department-plans/count-plans' \
+#   -H 'accept: application/json' \
+#   -H 'apiKey: 45d5929101e1d76cf3e54ff8ed4624898ebf1fd8619e5b66c15c938e83e4b533' | jq
+
+# # status = draft
+# curl -X 'POST' \
+#   'http://localhost:9010/api/bss/department-plans' \
+#   -H 'accept: application/json' \
+#   -H 'apiKey: 45d5929101e1d76cf3e54ff8ed4624898ebf1fd8619e5b66c15c938e83e4b533' \
+#   -H 'Content-Type: application/json' \
+#   -d '{"filters":{"status":["draft"]},"sort":[],"page":1,"pageSize":10}
+# ' | jq
+
+# # status = pending + tab = sender
+# curl -X 'POST' \
+#   'http://localhost:9010/api/bss/department-plans' \
+#   -H 'accept: application/json' \
+#   -H 'apiKey: 45d5929101e1d76cf3e54ff8ed4624898ebf1fd8619e5b66c15c938e83e4b533' \
+#   -H 'Content-Type: application/json' \
+#   -d '{"filters":{"status":["pending"],"tab":"sender"},"sort":[],"page":1,"pageSize":10}
+# ' | jq
+
+# # status = pending + tab = reviewer
+# curl -X 'POST' \
+#   'http://localhost:9010/api/bss/department-plans' \
+#   -H 'accept: application/json' \
+#   -H 'apiKey: 45d5929101e1d76cf3e54ff8ed4624898ebf1fd8619e5b66c15c938e83e4b533' \
+#   -H 'Content-Type: application/json' \
+#   -d '{"filters":{"status":["pending"],"tab":"reviewer"},"sort":[],"page":1,"pageSize":10}
+# ' | jq
+
+
+
+# # status = approved + tab = reviewer
+# curl -X 'POST' \
+#   'http://localhost:9010/api/bss/department-plans' \
+#   -H 'accept: application/json' \
+#   -H 'apiKey: 45d5929101e1d76cf3e54ff8ed4624898ebf1fd8619e5b66c15c938e83e4b533' \
+#   -H 'Content-Type: application/json' \
+#   -d '{"filters":{"status":["approved"],"tab":"reviewer"},"sort":[],"page":1,"pageSize":10}
+# ' | jq
+
+
+
+ 
+# # status = approved + tab = approver
+# curl -X 'POST' \
+#   'http://localhost:9010/api/bss/department-plans' \
+#   -H 'accept: application/json' \
+#   -H 'apiKey: 45d5929101e1d76cf3e54ff8ed4624898ebf1fd8619e5b66c15c938e83e4b533' \
+#   -H 'Content-Type: application/json' \
+#   -d '{"filters":{"status":["approved"],"tab":"approver"},"sort":[],"page":1,"pageSize":10}
+# ' | jq 
+
+
+            
+# # status = completed
+# curl -X 'POST' \
+#   'http://localhost:9010/api/bss/department-plans' \
+#   -H 'accept: application/json' \
+#   -H 'apiKey: 45d5929101e1d76cf3e54ff8ed4624898ebf1fd8619e5b66c15c938e83e4b533' \
+#   -H 'Content-Type: application/json' \
+#   -d '{"filters":{"status":["completed"]},"sort":[],"page":1,"pageSize":10}
+# ' | jq
+
+# # status = approved + rejected + completed
+# curl -X 'POST' \
+#   'http://localhost:9010/api/bss/department-plans' \
+#   -H 'accept: application/json' \
+#   -H 'apiKey: d6b8ca7d11fcfd670707e0f33d8e454b2366af8216c2e05536f49ed9c343e8ea' \
+#   -H 'Content-Type: application/json' \
+#   -d '{"filters":{"status":["approved","rejected","completed"]},"sort":[],"page":1,"pageSize":10}
+# ' | jq
---
+++ ngrok.md
@@ -0,0 +1,10 @@
+ngrok config add-authtoken 2u3hajOXutNEVxeuuI4PMWJofuO_4ZKhAoVLMZT4Vnzbmo4H4
+
+<!-- http://localhost:9010/swagger-ui/index.html -->
+
+ngrok http --url=terminally-natural-crawdad.ngrok-free.app 80
+
+ngrok http 9010
+https://80ea-2405-4803-ff97-9db0-b5db-74aa-a03e-49e7.ngrok-free.app/swagger-ui/index.html
+
+<!-- http://localhost:9010/swagger-ui/index.html -->
